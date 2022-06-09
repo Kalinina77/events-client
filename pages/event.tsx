e3eb12c6afe0b3e483a6a217a64fe50acb44c518
Kalinina77 <isip_a.v.kalinina@mpt.ru>
2022-06-09 13:35:52 +0300

    fix unloading

--- pages/event.tsx
+++ pages/event.tsx
@@ -140,7 +140,7 @@ const Event = () => {
           <a
             href="unloadingEvent"
             className="btn btn-outline-success "
-            type="submit"
+          
           >
             Выгрузка
           </a>
